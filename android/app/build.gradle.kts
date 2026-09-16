10c3fd8883e7231639d3c82c99874ea0d25b309c
Fakhri Aditia Rahman <fakhri@student.unp.ac.id>
2026-09-16 14:58:02 +0700

    chore: bump versionName ke 1.2.6 versionCode 16 untuk rilis

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.monodev.monopos"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.2.5"
+        versionCode = 16
+        versionName = "1.2.6"
         multiDexEnabled = true
     }
 
